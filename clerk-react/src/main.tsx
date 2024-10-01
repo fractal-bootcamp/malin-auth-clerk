49f3c018b3d13344ad0ee98a6c229410be4a5822
kmankan <malink027@gmail.com>
2024-10-01 12:04:42 -0400

    integrated clerk into react frontend and setup React Router routes following tutorial at https://clerk.com/docs/references/react/add-react-router"

--- clerk-react/src/main.tsx
+++ clerk-react/src/main.tsx
@@ -1,8 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Import the layouts
+import RootLayout from './layouts/root-layout'
+import DashboardLayout from './layouts/dashboard-layout'
+
+// Import the components
+import IndexPage from './routes'
+import ContactPage from './routes/contact'
+import SignInPage from './routes/sign-in'
+import SignUpPage from './routes/sign-up'
+import DashboardPage from './routes/dashboard'
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -11,8 +21,27 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <IndexPage /> },
+      { path: '/contact', element: <ContactPage /> },
+      { path: '/sign-in', element: <SignInPage /> },
+      { path: '/sign-up', element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: 'dashboard',
+        children: [
+          { path: '/dashboard', element: <DashboardPage /> },
+        ]
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
